65779f399ba0e5687b8bedfd4515f08894a6812a
kellykang-tech <kellykang13@gmail.com>
2021-09-28 18:45:34 +0900

    :memo: Add: 스프링 없이 의존성 주입 - 생성자를 통한 의존성 주입

---
+++ ReadingRecord/practiceCode/objectOrientedPrac/ExpertSpring30/src/main/java/com/heaven/mvc/expert001_02/Driver.java
@@ -0,0 +1,12 @@
+package com.heaven.mvc.expert001_02;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        Tire tire = new KoreaTire();
+//        Tire tire = new AmericaTire();
+        Car car = new Car(tire);
+
+        System.out.println(car.getTireBrand());
+    }
+}
---
+++ ReadingRecord/practiceCode/objectOrientedPrac/ExpertSpring30/src/main/java/com/heaven/mvc/expert001_02/Tire.java
@@ -0,0 +1,5 @@
+package com.heaven.mvc.expert001_02;
+
+public interface Tire {
+    String getBrand();
+}
